613140c1a81dbcc604d5b6727e74e30907228db1
YJ <yj@example.com>
2026-03-23 22:17:20 +0900

    소스 추가

---
+++ src/main/java/me/my_library_system/MyLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package me.my_library_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLibrarySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/my_library_system/MyLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.my_library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
